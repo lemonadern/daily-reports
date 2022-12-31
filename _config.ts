35f02ba42ba5ece6dc1cf551280c023f8fa6ac2d
lemonadern <taishi.develop@gmail.com>
2023-01-01 05:48:53 +0900

    fix: add infomation of site location

--- _config.ts
+++ _config.ts
@@ -1,11 +1,13 @@
 import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
 import windi_css from "lume/plugins/windi_css.ts";
+import { SITE_URL } from "./src/_constants/constants.ts";
 
 import { windiConfig } from "./windi_config.ts";
 
 const site = lume({
   src: "./src",
+  location: new URL(SITE_URL),
 });
 
 site.use(jsx())
