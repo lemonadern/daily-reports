8901a6fdd2d93d9803b197eb86cd4751026c8c5f
lemonadern <taishi.develop@gmail.com>
2023-01-01 01:21:25 +0900

    feat: header

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -1,7 +1,31 @@
+import { sourceCodeLink } from "constants/links.ts";
+
 export const Header = () => {
   return (
-    <header class="bg-cyan-100">
-      <a href="/">lemonadern's daily report</a>
+    <header class="border px-16 py-4">
+      <div class="flex items-center justify-between mx-auto">
+        <div class="flex items-center gap-4 text-xl">
+          <a href="/">
+            <img
+              class="w-8 rounded-1/2"
+              src="/assets/profile.webp"
+              alt="Profile image"
+            />
+          </a>
+          lemonadern nightly
+          <img class="w-6 fill-sky-800" src="/assets/nights-stay-moon.svg" />
+        </div>
+
+        <a href={sourceCodeLink}>
+          <img
+            alt="GitHub Icon"
+            class="w-6"
+            src="https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/github.svg"
+          />
+        </a>
+      </div>
     </header>
   );
 };
+
+export default Header;
---
+++ src/_constants/links.ts
@@ -0,0 +1,6 @@
+export const twitterLink = "https://twitter.com/lemonadern";
+export const githubLink = "https://github.com/lemonadern";
+
+export const zennLink = "https://zenn.dev/lemonadern";
+
+export const sourceCodeLink = "github.com/";
