fc98d36bfc2e64e9a222d88f2f30ad5305e74933
lemonadern <taishi.develop@gmail.com>
2022-12-31 18:57:14 +0900

    feat: add windi_css

--- _config.ts
+++ _config.ts
@@ -1,11 +1,13 @@
 import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
-
+import windi_css from "lume/plugins/windi_css.ts";
 const site = lume({
   src: "./src",
 });
 
-site.use(jsx());
+site.use(jsx())
+  .use(windi_css());
+
 site.copy([".png", ".jpg", ".gif", ".svg"]);
 
 export default site;
