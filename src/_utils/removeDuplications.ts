002bc7aa95499e7d9dac451c3dcb064ee33e6a02
lemonadern <taishi.develop@gmail.com>
2023-01-01 04:39:34 +0900

    chore: update dependency

--- src/_utils/removeDuplications.ts
+++ src/_utils/removeDuplications.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.170.0/testing/asserts.ts";
 
 export const removeDuplications = (str: string) => {
   const arr = [...str];
