ffc55f9cd929543b0823b2eac51d66a68c698630
lemonadern <taishi.develop@gmail.com>
2023-01-02 23:26:20 +0900

    feat: implement top page (minimum implementaion)

--- _config.ts
+++ _config.ts
@@ -1,6 +1,8 @@
 import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
 import windi_css from "lume/plugins/windi_css.ts";
+import date from "lume/plugins/date.ts";
+
 import { SITE_URL } from "./src/_constants/constants.ts";
 
 import { windiConfig } from "./windi_config.ts";
@@ -11,7 +13,8 @@ const site = lume({
 });
 
 site.use(jsx())
-  .use(windi_css({ config: windiConfig }));
+  .use(windi_css({ config: windiConfig }))
+  .use(date());
 
 site.copy([".png", ".jpg", ".gif", ".svg", ".webp"]);
 
