b9bb02ec3192151ee651a6bafcc9bc29bf892f34
lemonadern <taishi.develop@gmail.com>
2023-01-03 05:20:59 +0900

    feat: support md urls

--- _config.ts
+++ _config.ts
@@ -1,5 +1,7 @@
 import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
+import relativeUrls from "lume/plugins/relative_urls.ts";
+import resolveUrls from "lume/plugins/resolve_urls.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
 import windi_css from "lume/plugins/windi_css.ts";
 import date from "lume/plugins/date.ts";
@@ -14,6 +16,8 @@ const site = lume({
 });
 
 site.use(basePath())
+  .use(relativeUrls())
+  .use(resolveUrls())
   .use(jsx())
   .use(windi_css({ config: windiConfig }))
   .use(date());
