65d4ffe1a620cf35d78c9628ff2eef4c5d83fb05
lemonadern <taishi.develop@gmail.com>
2023-01-03 05:25:27 +0900

    change: add title(date) attribute to nightlies

--- create_todays_nightly.sh
+++ create_todays_nightly.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-template="---\noverview: overview\n---\n\n# やったこと\n- "
+today=`date +%Y-%m-%d`
+template="---\ntitle: $today\noverview: overview\n---\n\n# やったこと\n- "
 
 mkdir ./src/nightlies/`date +%Y/%m/%d`
-echo $template > src/nightlies/`date +%Y/%m/%d`/`date +%Y-%m-%d`_index.md 
+echo $template > src/nightlies/`date +%Y/%m/%d`/$today\_index.md 
