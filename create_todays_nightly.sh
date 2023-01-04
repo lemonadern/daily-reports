aef241ad85db82395e2d81bb8c28d877d6b6203b
lemonadern <taishi.develop@gmail.com>
2023-01-04 22:24:00 +0900

    change: rename nightlies/ to nightly/

--- create_todays_nightly.sh
+++ create_todays_nightly.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 today=`date +%Y-%m-%d`
+nightly_dir=nightly
 template="---\ntitle: $today\noverview: overview\n---\n\n# やったこと\n- "
 
-mkdir ./src/nightlies/`date +%Y/%m/%d`
-echo $template > src/nightlies/`date +%Y/%m/%d`/$today\_index.md 
+mkdir ./src/$nightly_dir/`date +%Y/%m/%d`
+echo $template > src/$nightly_dir/`date +%Y/%m/%d`/$today\_index.md 
