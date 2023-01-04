512d5cc9f8f7b13019119e2490ea8a78a60dae0a
lemonadern <taishi.develop@gmail.com>
2023-01-04 22:10:44 +0900

    change: set 'target=_blank' to a tag in header

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -1,15 +1,23 @@
 import { SOURCECODE_LINK } from "constants/links.ts";
-import { SITE_NAME, SITE_URL } from "constants/constants.ts";
+import { SITE_NAME } from "constants/constants.ts";
 
 export const Header = () => {
   return (
     <header class="border-b px-6 md:px-8 py-4 bg-fresh-marine-blue">
       <div class="flex items-center justify-between">
-        <a href="/" class="flex items-center gap-4 text-xl font-title text-white">
+        <a
+          href="/"
+          class="flex items-center gap-4 text-xl font-title text-white"
+        >
           {SITE_NAME}
         </a>
 
-        <a href={SOURCECODE_LINK} class="<sm:hidden">
+        <a
+          href={SOURCECODE_LINK}
+          target="_blank"
+          rel="noopener noreferrer"
+          class="<sm:hidden"
+        >
           <img
             alt="GitHub Icon"
             class="w-4 <sm:hidden"
