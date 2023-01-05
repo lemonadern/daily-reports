2b2061d83b31bca035cc100b14eb65c60d0adfe6
lemonadern <taishi.develop@gmail.com>
2023-01-05 22:59:29 +0900

    feat: add meta tags

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@ import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
 import relativeUrls from "lume/plugins/relative_urls.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
+import metas from "lume/plugins/metas.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
 import windi_css from "lume/plugins/windi_css.ts";
 import date from "lume/plugins/date.ts";
@@ -20,6 +21,7 @@ const site = lume({
 site.use(basePath())
   .use(relativeUrls())
   .use(resolveUrls())
+  .use(metas())
   .use(jsx())
   .use(windi_css({ config: windiConfig }))
   .use(date())
