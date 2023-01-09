cee79f60f8ac390b33f454462295b87dba4193b2
lemonadern <taishi.develop@gmail.com>
2023-01-09 15:46:31 +0900

    style: format

--- markdown_config.ts
+++ markdown_config.ts
@@ -3,16 +3,20 @@ import footnote from "npm:markdown-it-footnote";
 import linkAttr from "npm:markdown-it-link-attributes";
 
 export const markdownItConfig = {
-  plugins: [emoji, footnote, [linkAttr, {
-    // deno-lint-ignore no-explicit-any
-    matcher(href: any) {
-      return href.startsWith("https:");
-    },
-    attrs: {
-      target: "_blank",
-      rel: "noopener noreferrer",
-    },
-  }]],
+  plugins: [
+    emoji,
+    footnote,
+    [linkAttr, {
+      // deno-lint-ignore no-explicit-any
+      matcher(href: any) {
+        return href.startsWith("https:");
+      },
+      attrs: {
+        target: "_blank",
+        rel: "noopener noreferrer",
+      },
+    }],
+  ],
   keepDefaultPlugins: true,
   options: {
     html: true,
