16058e261b77b1a5a6e4cd5fce338e1367ee2a33
lemonadern <taishi.develop@gmail.com>
2023-01-14 12:53:33 +0900

    feat(deno task): 'create new nightly' task

---
+++ create_nightly.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+iso_date=`date +%Y-%m-%d`
+dir_date=`date +%Y/%m/%d`
+
+if [ $# -eq 1 ]; then
+  # overwrite dates
+  iso_date=`date +%Y-%m-%d -d "$1"`
+  dir_date=`date +%Y/%m/%d -d "$1"`
+fi
+
+nightly_dir=nightly
+template="---\ntitle: \"$iso_date\"\noverview: overview\n---\n\n# やったこと\n- "
+
+echo $iso_date
+mkdir ./src/$nightly_dir/$dir_date
+echo $template > src/$nightly_dir/$dir_date/$iso_date\_index.md 
