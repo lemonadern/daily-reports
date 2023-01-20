13a06bc6793a7a4fbb024c6280110c08b873ca7c
lemonadern <taishi.develop@gmail.com>
2023-01-20 22:24:26 +0900

    fix: article header padding style

--- src/_components/articleHeader.tsx
+++ src/_components/articleHeader.tsx
@@ -4,8 +4,8 @@ type Props = {
 };
 
 export const ArticleHeader = ({ title, published_at }: Props) => (
-  <header class="flex flex-col items-center gap-4 py-16">
-    <h1 class="text-3xl font-extrabold">
+  <header class="flex flex-col items-center gap-4 py-16 px-6">
+    <h1 class="text-3xl font-bold">
       {title}
     </h1>
     <p class="text-md opacity-70">{published_at}</p>
