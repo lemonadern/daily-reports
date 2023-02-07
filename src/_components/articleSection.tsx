cd61f365a0dc80915557247ea0e87a685301b93d
lemonadern <taishi.develop@gmail.com>
2023-02-07 14:01:03 +0900

    style: format

--- src/_components/articleSection.tsx
+++ src/_components/articleSection.tsx
@@ -1,11 +1,11 @@
-import { ComponentChildren } from "npm:preact"
+import { ComponentChildren } from "npm:preact";
 
 type Props = {
-  children: ComponentChildren
-}
+  children: ComponentChildren;
+};
 
 export const ArticleSection = ({ children }: Props) => (
   <article class="md-prose max-w-1/1 px-8 py-8  sm:(max-w-8/10 rounded-xl) md:(max-w-7/10) lg:(max-w-6/10 p-12) xl:(max-w-5/10 p-16) mx-auto bg-white">
     {children}
   </article>
-)
+);
